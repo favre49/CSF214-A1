eb49dd13ecc04bb2253fce6dff05b33c5df31d40
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2019-10-12 20:47:50 +0530

    Create Q2d.pl

---
+++ Q2/Q2d.pl
@@ -0,0 +1,4 @@
+remove_every_other(X,Z):-accremove_every_other(X,Z,0).
+accremove_every_other([],[],_).
+accremove_every_other([H|L],[H|Y],Count):-0 is mod(Count,2) , NewCount is Count + 1, accremove_every_other(L,Y,NewCount).
+accremove_every_other([_|L],Y,Count):-1 is mod(Count,2),NewCount is Count + 1,accremove_every_other(L,Y,NewCount).
