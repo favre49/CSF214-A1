780511ed931d56b027ace1228ee1ea1d64c6a64b
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2019-10-12 20:46:21 +0530

    Create Q2c.pl

---
+++ Q2/Q2c.pl
@@ -0,0 +1,4 @@
+remove_nth(N,X,Z):-accremove_nth(N,X,Z,1).
+accremove_nth(N,[],[],Count):-Count>=N.
+accremove_nth(N,[H|L],[H|Y],Count):- Count =\= N, NewCount is Count + 1, accremove_nth(N,L,Y,NewCount).
+accremove_nth(N,[_|L],Y,N):-NewCount is N + 1,accremove_nth(N,L,Y,NewCount).
