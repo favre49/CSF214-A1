dfa0d497c8663acef8b4bb653721f0bb7cefff5f
Rahul Prabhu <cupertinorp@gmail.com>
2019-10-13 14:34:45 +0530

    Fixed case for Q2c where it does not fail if N is more than length

--- Q2/Q2c.pl
+++ Q2/Q2c.pl
@@ -1,11 +1,16 @@
 remove_nth(N,X,Z):-
+    length(X,Len),
+    Len>=N,
     accremove_nth(N,X,Z,1).
 
 accremove_nth(N,[],[],Count):-
     Count>=N.
 
 accremove_nth(N,[H|L],[H|Y],Count):- 
-    Count =\= N, NewCount is Count + 1, accremove_nth(N,L,Y,NewCount).
+    Count =\= N,
+    NewCount is Count + 1,
+    accremove_nth(N,L,Y,NewCount).
 
 accremove_nth(N,[_|L],Y,N):-
-    NewCount is N + 1,accremove_nth(N,L,Y,NewCount).
+    NewCount is N + 1,
+    accremove_nth(N,L,Y,NewCount).
